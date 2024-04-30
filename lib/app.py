6a33304d921bac4fa2bb81396cac9548825c2358
Jared Patterson <jpwesley34@gmail.com>
2024-04-30 10:09:31 -0700

    Passes tests.

---
+++ lib/app.py
@@ -0,0 +1,4 @@
+# The pound sign is the equivalent to // in JavaScript files.
+print("Hello World! Pass this test, please.")
+# The \n at the end of the last one is the "newline" thingy.
+# In Python, instead of camelCasing, it uses snake_casing with underscores between words.
